5f007abdc1736543844267ae4072169fd9231e00
PsionicPotato <psionicpotato@gmail.com>
2014-10-04 15:19:45 -0700

    new stuff!
    money, pathing, window things

---
+++ TowerDefenseMayhem/TowerDefenseMayhem/Money.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TowerDefenseMayhem
+{
+    class Money
+    {
+        int Cash;
+
+        public Money()
+        {
+            Cash = 1000;
+        }
+
+        public bool RequestPurchase(int amount)
+        {
+            if (amount > Cash)
+            {
+                System.Windows.MessageBox.Show("You can't afford this tower.");
+                return false;
+            }
+            else
+            {
+                Cash -= amount;
+                return true;
+            }
+        }
+
+        public void AddMoney(int amount)
+        {
+            Cash += amount;
+        }
+    }
+}
